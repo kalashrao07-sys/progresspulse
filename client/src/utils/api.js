80a117ff0efbf6c354e01f41d1a14e27d5b47b3b
Kalash Rao <kalash.rao07@gmail.com>
2026-05-30 10:28:13 +0530

    Update API base URL to a fixed endpoint

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api'
+const api = axios.create({ 
+  baseURL: 'https://progresspulse-2.onrender.com/api'
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('pp_token');
