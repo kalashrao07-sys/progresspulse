d7c9439edd0bd4b6124bfc0139dc9b91d091decf
Kalash Rao <kalash.rao07@gmail.com>
2026-05-30 09:58:25 +0530

    Update API base URL configuration to use environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || '/api'
+});
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('pp_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
